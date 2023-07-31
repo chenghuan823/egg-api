a8fba99be7cd105dbe139433496423416cdc9beb
chenghuan <508231435@qq.com>
2023-07-31 22:02:16 +0800

    where条件查询单个

--- app/controller/user.js
+++ app/controller/user.js
@@ -19,13 +19,21 @@ class UserController extends Controller {
     //拿到路由参数
     let id=parseInt(ctx.params.id)
     //通过主键查询单个数据
-    let detail=await ctx.model.Users.findByPk(id)
-    if(!detail){
-      return ctx.body={
-        msg:'fail',
-        data:'用户不存在'
+    // let detail=await ctx.model.Users.findByPk(id)
+    // if(!detail){
+    //   return ctx.body={
+    //     msg:'fail',
+    //     data:'用户不存在'
+    //   }
+    // }
+
+    //查询单个
+    let detail=await ctx.model.Users.findOne({
+      where:{
+        id,
+        sex:'女'
       }
-    }
+    })
     ctx.body={
       msg:'ok',
       data:detail
